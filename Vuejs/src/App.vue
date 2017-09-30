046c274349e99bb075a94259325f4003d777bf93
0_o <summer.wxy@gmail.com>
2017-09-30 13:32:37 +0800

    kickoff

---
+++ Vuejs/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <Header></Header>
+    <router-view></router-view>
+    <Footer></Footer>
+  </div>
+</template>
+
+<script>
+import Header from './components/commons/Header'
+import Footer from './components/commons/Footer'
+export default {
+  name: 'app',
+  components: {
+    Header,
+    Footer
+  }
+}
+</script>
+
+<style>
+/*
+    <img src="./assets/logo.png">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+*/
+</style>
